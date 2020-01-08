adf2bb66dd22fe24695c3d7266db72cebe7982c6
BenDie0 <58610677+BenDie0@users.noreply.github.com>
2020-01-08 13:18:39 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,61 @@
+let juan = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . . f f . . . . 
+. . . . . f . f . f . f . . . . 
+. . . . . f f . . . . f . . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . f f f . . . f . . . f f f . 
+. . . . f f f f f f f f f . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . . . f . . . . . . . 
+. . . . . . f f . f f . . . . . 
+. . . . . f f . . . f f . . . . 
+`, SpriteKind.Player)
+scene.setBackgroundColor(14)
+for (let index = 0; index < 6; index++) {
+    pause(100)
+    juan.y += 4
+    pause(100)
+    juan.x += 4
+}
+for (let index = 0; index < 6; index++) {
+    pause(100)
+    juan.y += -4
+    pause(100)
+    juan.x += -4
+}
+let mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f f f f f f f f f . . . 
+. . . f . . . . . . . . f . . . 
+. . . f . . f . . f . . f . . . 
+. . . f . . . . . . . . f . . . 
+. . . f . f . . . . . . f . . . 
+. . . f . . f f f f f f f f f f 
+. . f f . . . . . . . . f . . . 
+. . . f f f f f f f f f f . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . f f f f f f f f f f f f f f 
+. . . . . . . . f . . . . . . . 
+. . . . . . . . f . . . . . . . 
+`, SpriteKind.Player)
+for (let index = 0; index < 6; index++) {
+    pause(100)
+    mySprite.y += 4
+    pause(100)
+    mySprite.x += -4
+}
+for (let index = 0; index < 6; index++) {
+    pause(100)
+    mySprite.y += -4
+    pause(100)
+    mySprite.x += 4
+}
